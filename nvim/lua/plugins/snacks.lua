38d838640fa3310a834dc99475f526fb6e04cae4
Josh Chan <redgreenpro2@gmail.com>
2026-02-16 01:43:04 +1000

    Added nvim

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+   "folke/snacks.nvim",
+   priority = 1000,
+   lazy = false,
+   ---@type snacks.Config
+   opts = {
+      ---@type table<string, snacks.win.Config>
+      styles = {
+	      terminal = { height = 0.2 },
+	      explorer = { width = 0.3}
+      }
+   }
+
+}
+
